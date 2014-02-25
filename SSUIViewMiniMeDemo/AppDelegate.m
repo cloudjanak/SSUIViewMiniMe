d364157b089afa649ee5bd5ae6370a868fd6e0c8
David Jardón <david.jardon@sopragroup.com>
2014-02-25 12:08:11 +0100

    Update

---
+++ SSUIViewMiniMeDemo/AppDelegate.m
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.m
+//  SSUIViewMiniMeDemo
+//
+//  Created by i7Mac on 12/15/13.
+//  Copyright (c) 2013 Segev. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
+{
+        [[UIApplication sharedApplication] setStatusBarHidden:YES];
+    return YES;
+}
+							
+- (void)applicationWillResignActive:(UIApplication *)application
+{
+
+}
+
+- (void)applicationDidEnterBackground:(UIApplication *)application
+{
+
+}
+
+- (void)applicationWillEnterForeground:(UIApplication *)application
+{
+
+}
+
+- (void)applicationDidBecomeActive:(UIApplication *)application
+{
+
+}
+
+- (void)applicationWillTerminate:(UIApplication *)application
+{
+}
+
+@end
